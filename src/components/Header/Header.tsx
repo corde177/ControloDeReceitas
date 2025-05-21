27cfb588a9f0a5f0c9421c30444b96572d5ce7fc
¨Cordeiro <cordeiroluisff@gmail.com>
2025-05-21 21:55:10 +0100

    feat: Criando um modal acessivel, usando a biblioteca radi-ui

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,30 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
+import {
+  HeaderContainer,
+  HeaderContent,
+  NewTransactionButton,
+} from "./style";
 import LogoImg from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={LogoImg} alt="" />
-        <NewTransactionButton>
-           Nova Transação
-        </NewTransactionButton>
+        <img src={LogoImg} alt="Logo" />
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
